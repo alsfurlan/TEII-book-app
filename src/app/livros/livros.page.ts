25c3f7b72770f99a1b282128c0d309afb41734c9
Anderson Luis Furlan <alsfurlan@gmail.com>
2020-05-05 20:11:09 -0300

    Adicionada funcionalidade de exclusão de livros

--- src/app/livros/livros.page.ts
+++ src/app/livros/livros.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LivroService } from '../services/livro.service';
 import { Livro } from '../models/livro.interface';
-import { LoadingController } from '@ionic/angular';
+import { LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-livros',
@@ -14,20 +14,52 @@ export class LivrosPage implements OnInit {
 
   constructor(
     private livroService: LivroService,
-    private loadingController: LoadingController
+    private loadingController: LoadingController,
+    private alertController: AlertController
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   ionViewWillEnter() {
     this.listar();
   }
 
   async listar() {
-    const busyLoader = await this.loadingController.create({message: 'Carregando livros...'});
-    busyLoader.present();
+    const busyLoader = await this.iniciarCarregamento('Carregando livros ...');
     
     this.livros = await this.livroService.getLivros().toPromise();
     busyLoader.dismiss();
+  }
+
+  async confirmacaoExclusao(livro: Livro) {
+    const alerta = await this.alertController.create({
+      header: 'Confirmação de exclusão',
+      message: `Deseja excluir o livro ${livro.titulo}?`,
+      buttons: [
+        {
+          text: 'Sim',
+          handler: () => this.excluir(livro)
+        },
+        {
+          text: 'Não'
+        }
+      ]
+    });
+    alerta.present();       
+  }
+
+  private async excluir(livro: Livro) {
+    const busyLoader = await this.iniciarCarregamento('Excluíndo ...');
+    
+    this.livroService.excluir(livro).subscribe(() => {
+      this.listar()
+      busyLoader.dismiss();
+    });
+  }
+
+  private async iniciarCarregamento(mensagem: string) {
+    const busyLoader = await this.loadingController.create({ message: mensagem });
+    busyLoader.present();
+    return busyLoader;
   }
 }
--- src/app/services/livro.service.ts
+++ src/app/services/livro.service.ts
@@ -6,14 +6,18 @@ import { Livro } from '../models/livro.interface';
   providedIn: 'root'
 })
 export class LivroService {
-
+  
   private URI = 'http://localhost:3000/livros';
-
+  
   constructor(
     private httpClient:HttpClient
   ) { }
-
+    
   getLivros() {
     return this.httpClient.get<Livro[]>(this.URI);
+  }
+
+  excluir(livro: Livro) {
+    return this.httpClient.delete(`${this.URI}/${livro.id}`);
   }
 }
